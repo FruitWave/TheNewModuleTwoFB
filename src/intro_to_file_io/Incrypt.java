8b035eb1d5d5dd63870c2a43ffc500388d60fd8c
League <league@WorthyMini-6.attlocal.net>
2017-11-30 19:41:37 -0800

    Chkpt1
    Learned hexadecimals (base 16) today! fun!

--- src/intro_to_file_io/Incrypt.java
+++ src/intro_to_file_io/Incrypt.java
@@ -1,5 +1,33 @@
 package intro_to_file_io;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Base64;
+
+import javax.swing.JOptionPane;
+
 public class Incrypt {
+
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("src/intro_to_file_io/incrpt.txt");
+			String semic = JOptionPane.showInputDialog("Enter Text");
+
+			/*
+			 * NOTE: To append to a file that already exists, add true as a second parameter
+			 * when calling the FileWriter constructor. (e.g. FileWriter fw = new
+			 * FileWriter("src/intro_to_file_io/test2.txt", true);)
+			 */
+
+			fw.write("\n " + Base64.getEncoder().encodeToString(semic.getBytes()));
+			System.out.println(Base64.getEncoder().encodeToString(semic.getBytes()));
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
